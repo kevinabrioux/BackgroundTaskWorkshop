a6b14a01979460ccecef901a60cd81f47f1d2162
Kevin ABRIOUX <kevin.abrioux@gmail.com>
2024-05-16 15:37:01 +0200

    background

--- BackgroundTaskWorkshop/BackgroundTaskWorkshopApp.swift
+++ BackgroundTaskWorkshop/BackgroundTaskWorkshopApp.swift
@@ -6,12 +6,49 @@
 //
 
 import SwiftUI
+import BackgroundTasks
 
 @main
 struct BackgroundTaskWorkshopApp: App {
+    
+    let taskIdentifier = "com.kevinabrioux.backgroundtask.refresh"
+    
+    func handleAppRefresh(task: BGAppRefreshTask) {
+       // Schedule a new refresh task.
+       scheduleAppRefresh()
+     }
+    
+    func scheduleAppRefresh() {
+        print("===>>> scheduleAppRefresh")
+       let request = BGProcessingTaskRequest(identifier: taskIdentifier)
+
+        // Fetch no earlier than 15 minutes from now.
+       request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
+            
+       do {
+          try BGTaskScheduler.shared.submit(request)
+           print("Background task scheduled successfully.")
+
+           // e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"com.kevinabrioux.backgroundtask.refresh"]
+
+       } catch {
+          print("Could not schedule app refresh: \(error)")
+       }
+    }
+    
+    func trigger() async {
+        print("===>>> in trigger")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    scheduleAppRefresh()
+                }
+        }.backgroundTask(.appRefresh(taskIdentifier)) { task in
+            print("===>>> in .appRefresh")
+            await trigger()
         }
     }
 }
