25efe5edb3e41d4dfd62ca75823e6149659c1956
Kevin ABRIOUX <kevin.abrioux@gmail.com>
2024-05-16 10:25:11 +0200

    init_project

---
+++ BackgroundTaskWorkshop/BackgroundTaskWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTaskWorkshopApp.swift
+//  BackgroundTaskWorkshop
+//
+//  Created by Kevin ABRIOUX on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTaskWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackgroundTaskWorkshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BackgroundTaskWorkshop
+//
+//  Created by Kevin ABRIOUX on 16/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
